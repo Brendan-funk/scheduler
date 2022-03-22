6ef06e4ee21b334c152b2e8e21251f47a218dbc0
denda <brendanzfunk@gmail.com>
2022-03-22 12:37:58 -0700

    adds the daylist js file and adds the storybook for daylist

---
+++ src/components/DayList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+export default function DayList (props) {
+  const dayListItemArray = props.days.map(x=> <DayListItem
+    key={x.id}
+    name={x.name}
+    spots={x.name}
+    selected={x.selected}
+    setDay={props.setDay}
+    />)
+  return( <ul>
+    {dayListItemArray}
+  </ul>)
+}
