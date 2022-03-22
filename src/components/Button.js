22e6e66ee8992887f54e68e2081e0fd95069132c
denda <brendanzfunk@gmail.com>
2022-03-22 12:03:11 -0700

    first commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger,
+   })
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
+ 
